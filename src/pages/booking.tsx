ca3d84e49fd769db50a11a50b243117e2a2bffe9
Mike Hacker <mhackersu@gmail.com>
2025-01-18 21:15:42 -0600

    some html fixup

--- src/pages/booking.tsx
+++ src/pages/booking.tsx
@@ -17,7 +17,7 @@ export default function Booking() {
         </div>
         <form className="max-w-sm mx-auto">
           <div className="mb-5">
-            <label for="password" className="block mb-2 text-medium font-medium text-gray-900">Date Requested</label>
+            <label className="block mb-2 text-medium font-medium text-gray-900">Date Requested</label>
             <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
           </div>
           <div className="mb-5">
@@ -35,9 +35,9 @@ export default function Booking() {
             <div className="flex items-center h-5">
               <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
             </div>
-            <label for="remember" className="ms-2 text-sm font-medium text-gray-900">Agree to <Link href="/terms">Terms & Conditions</Link></label>
+            <label className="ms-2 text-sm font-medium text-gray-900">Agree to <Link href="/terms">Terms & Conditions</Link></label>
           </div>
-          <button type="login" className="text-white bg-slate-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Check Availability</button>
+          <button type="submit" className="text-white bg-slate-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Check Availability</button>
         </form>
         <br></br>
         <Footer />
