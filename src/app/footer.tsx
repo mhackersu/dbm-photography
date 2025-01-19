165dbb47c8f962eb57f1bfad133c6ead68e051ac
Mike Hacker <mhackersu@gmail.com>
2025-01-18 19:49:43 -0600

    abstract footer

---
+++ src/app/footer.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+import "../app/globals.css";
+
+export default function Footer() {
+  
+    return (
+      <div>
+        <div className="bg-green-800 text-white text-opacity-40 font-semibold uppercase text-xs tracking-widest bg-opacity-80 px-12">
+        <div className="container mx-auto grid grid-cols-4 py-24">
+            <div>
+                <div className="text-white opacity-50 text-4xl font-display">HOWDY PARTNERS</div>
+                <br></br>
+                <div>New Client Onboarding</div>
+            </div>
+            <div>
+                <div className="font-display text-white uppercase text-sm tracking-widest mb-6">Services</div>
+                <a href="#" className="block mb-4">Weddings & Portraits</a>
+                <a href="#" className="block mb-4">Commercial</a>
+                <a href="#" className="block mb-4">Reproduction</a>
+                <a href="#" className="block mb-4">Restoration</a>
+                <a href="#" className="block mb-4">Framing</a>
+            </div>
+            <div>
+                <div className="font-display text-white uppercase text-sm tracking-widest mb-6">Laboratories</div>
+                <a href="#" className="block mb-4">White House</a>
+                <a href="#" className="block mb-4">ProDPI</a>
+                <a href="#" className="block mb-4">H&H</a>
+                <a href="#" className="block mb-4">Custom</a>
+            </div>
+            <div>
+                <div className="font-display text-white uppercase text-sm tracking-widest mb-6">Products</div>
+                <a href="#" className="block mb-4">4K</a>
+                <a href="#" className="block mb-4">6K</a>
+                <a href="#" className="block mb-4">C41</a>
+                <a href="#" className="block mb-4">C120</a>
+            </div>
+        </div>
+        <div className="text-sm lg:text-base text-center font-heading font-light tracking-widest uppercase text-white opacity-75 pb-24">©2025 Design By DBMCO.NET</div>
+        </div>
+      </div>
+    );
+}
+
+
---
+++ src/pages/about.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import "../app/globals.css";
+import Footer from '@/app/footer';
+
+export default function About() {
+  
+    return (
+      <div>
+
+      {/* Header Menu */}
+        <div className="bg-gray-100 py-12 flex items-center justify-center uppercase tracking-widest text-xs text-gray-500">
+          <Link href="/" className="mx-4">Home</Link>
+          <a href="#" className="mx-4">Booking</a>
+          <Link href="/gallery" className="mx-4">Gallery</Link>
+          <a href="#" className="mx-4">Reservations</a>
+        </div>
+        {/* Centered Page Title & Subtitle */}
+        <div className="py-24 text-center">
+          <h1 className="text-4xl text-green-800 font-display uppercase tracking-widest">About Us</h1>
+          <div className="w-full max-w-xs h-px bg-gray-300 my-6 mx-auto"></div>
+          <h2 className="uppercase max-w-sm mx-auto font-heading font-light tracking-widest text-gray-500">
+          We value charity, courage, respect, and the heritage of our community. These principles inspire our work and shape our artistic approach.  
+Photography is our way of celebrating life and the human experience. Through our gallery, we showcase our dedication to craftsmanship and storytelling.  
+As wedding photographers, we are passionate about capturing the beauty and uniqueness of each couple's journey. Every wedding tells a one-of-a-kind story, and it is our honor to document these moments with care and precision. Your wedding day is a celebration, and we strive to preserve its magic through timeless, high-quality imagery.
+          </h2>
+        </div>
+
+        <Footer />
+      </div>
+    );
+}
